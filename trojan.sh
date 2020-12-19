3806412bd197d2ff79b113ca54b8b596a13c4487
pangbobi <49721270+pangbobi@users.noreply.github.com>
2020-12-19 20:50:27 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -169,10 +169,10 @@ installTrojan(){
 	expect {
 		"证书方式" { send "1"; exp_continue }
 		"书的域名" { send "$domain\n"; exp_continue }
-		"ysql连接" { sleep 1s; send "1"; exp_continue }
+		"ysql连接" { sleep 1; send "1"; exp_continue }
 		"义用户名" { send "root\n"; exp_continue }
 		"定义密码" { send "$uuid\n"; exp_continue }
-		"生成json" { sleep 1s; send "\n" }
+		"生成json" { sleep 1; send "\n" }
 	}
 	expect eof
 EOF
