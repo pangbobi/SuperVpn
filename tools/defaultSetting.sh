c3c9fe02fa3b147b9b8e69e14bad9738059d07fd
pangbobi <teddycool172@gmail.com>
2022-08-15 18:43:33 +0800

    添加配置文件初始化

--- tools/defaultSetting.sh
+++ tools/defaultSetting.sh
@@ -23,12 +23,26 @@ yello_font(){
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+# 信息提示字
 Info=$(green_font [信息])
 Error=$(red_font [错误])
 Tips=$(yello_font [注意])
 
+# 默认变量
 HOME_DIR="/root"
 INSATLL_DIR="/etc/SuperVpn"
 SCIRIPT_DIR="${INSATLL_DIR}/sh"
-STATUS_FILE="${INSATLL_DIR}/ServiceStatus.yml"
+STATUS_FILE="${HOME_DIR}/SuperVpnStatus.json"
 PROJECT_URL="https://raw.githubusercontent.com/pangbobi/SuperVpn/master"
+
+if [ ! -f $STATUS_FILE ];then
+    # 检查jq是否安装
+    source <(curl -sL ${PROJECT_URL}/tools/checkInstall.sh) apt jq
+    if [ $INSTALL_CHECK == "no" ];then
+        echo -e "${Error}$(green_font jq)安装失败，请先自行安装 jq"
+        exit 1;
+    fi
+
+    # 在 root 文件夹下生成空配置文件
+    echo "{}" > $STATUS_FILE
+fi
