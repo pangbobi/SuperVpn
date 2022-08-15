f105ab0e2156fb46665013bc6eae948c4284e4ce
pangbobi <teddycool172@gmail.com>
2022-08-16 00:05:21 +0800

    检测系统相关

---
+++ tools/system.sh
@@ -0,0 +1,190 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# 全局变量
+osCPU=""
+osArchitecture="amd64"
+osInfo=""
+osRelease=""
+osReleaseVersion=""
+osReleaseVersionNo=""
+osReleaseVersionNoShort=""
+osReleaseVersionCodeName="CodeName"
+osSystemPackage=""
+osSystemMdPath=""
+osSystemShell="bash"
+osVirtual=""
+
+# 检测系统架构
+checkArchitecture(){
+	# https://stackoverflow.com/questions/48678152/how-to-detect-386-amd64-arm-or-arm64-os-architecture-via-shell-bash
+
+	case $(uname -m) in
+		i386)   osArchitecture="386" ;;
+		i686)   osArchitecture="386" ;;
+		x86_64) osArchitecture="amd64" ;;
+		arm)    dpkg --print-architecture | grep -q "arm64" && osArchitecture="arm64" || osArchitecture="arm" ;;
+		aarch64)    dpkg --print-architecture | grep -q "arm64" && osArchitecture="arm64" || osArchitecture="arm" ;;
+		* )     osArchitecture="arm" ;;
+	esac
+}
+
+# 检测 CPU 种类
+checkCPU(){
+	osCPUText=$(cat /proc/cpuinfo | grep vendor_id | uniq)
+	if [[ $osCPUText =~ "GenuineIntel" ]]; then
+		osCPU="intel"
+    elif [[ $osCPUText =~ "AMD" ]]; then
+        osCPU="amd"
+    else
+        echo
+    fi
+}
+
+# 检测系统版本号
+getLinuxOSVersion(){
+    if [[ -s /etc/redhat-release ]]; then
+        osReleaseVersion=$(grep -oE '[0-9.]+' /etc/redhat-release)
+    else
+        osReleaseVersion=$(grep -oE '[0-9.]+' /etc/issue)
+    fi
+
+    # https://unix.stackexchange.com/questions/6345/how-can-i-get-distribution-name-and-version-number-in-a-simple-shell-script
+
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        source /etc/os-release
+        osInfo=$NAME
+        osReleaseVersionNo=$VERSION_ID
+
+        if [ -n "$VERSION_CODENAME" ]; then
+            osReleaseVersionCodeName=$VERSION_CODENAME
+        fi
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        osInfo=$(lsb_release -si)
+        osReleaseVersionNo=$(lsb_release -sr)
+
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        osInfo=$DISTRIB_ID
+        osReleaseVersionNo=$DISTRIB_RELEASE
+        
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        osInfo=Debian
+        osReleaseVersion=$(cat /etc/debian_version)
+        osReleaseVersionNo=$(sed 's/\..*//' /etc/debian_version)
+    elif [ -f /etc/redhat-release ]; then
+        osReleaseVersion=$(grep -oE '[0-9.]+' /etc/redhat-release)
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        osInfo=$(uname -s)
+        osReleaseVersionNo=$(uname -r)
+    fi
+
+    osReleaseVersionNoShort=$(echo $osReleaseVersionNo | sed 's/\..*//')
+}
+
+# 虚拟化类型
+virt_check(){
+	virtualx=$(dmesg) 2>/dev/null
+
+    if  [ "$(command -v dmidecode)" ]; then
+		sys_manu=$(dmidecode -s system-manufacturer) 2>/dev/null
+		sys_product=$(dmidecode -s system-product-name) 2>/dev/null
+		sys_ver=$(dmidecode -s system-version) 2>/dev/null
+	else
+		sys_manu=""
+		sys_product=""
+		sys_ver=""
+	fi
+	
+	if grep docker /proc/1/cgroup -qa; then
+	    osVirtual="Docker"
+	elif grep lxc /proc/1/cgroup -qa; then
+		osVirtual="Lxc"
+	elif grep -qa container=lxc /proc/1/environ; then
+		osVirtual="Lxc"
+	elif [[ -f /proc/user_beancounters ]]; then
+		osVirtual="OpenVZ"
+	elif [[ "$virtualx" == *kvm-clock* ]]; then
+		osVirtual="KVM"
+	elif [[ "$cname" == *KVM* ]]; then
+		osVirtual="KVM"
+	elif [[ "$cname" == *QEMU* ]]; then
+		osVirtual="KVM"
+	elif [[ "$virtualx" == *"VMware osVirtual Platform"* ]]; then
+		osVirtual="VMware"
+	elif [[ "$virtualx" == *"Parallels Software International"* ]]; then
+		osVirtual="Parallels"
+	elif [[ "$virtualx" == *VirtualBox* ]]; then
+		osVirtual="VirtualBox"
+	elif [[ -e /proc/xen ]]; then
+		osVirtual="Xen"
+	elif [[ "$sys_manu" == *"Microsoft Corporation"* ]]; then
+		if [[ "$sys_product" == *"osVirtual Machine"* ]]; then
+			if [[ "$sys_ver" == *"7.0"* || "$sys_ver" == *"Hyper-V" ]]; then
+				osVirtual="Hyper-V"
+			else
+				osVirtual="Microsoft osVirtual Machine"
+			fi
+		fi
+	else
+		osVirtual="Dedicated母鸡"
+	fi
+}
+
+# 检测系统发行版代号
+getLinuxOSRelease(){
+    if [[ -f /etc/redhat-release ]]; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    elif cat /etc/issue | grep -Eqi "debian|raspbian"; then
+        osRelease="debian"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="buster"
+    elif cat /etc/issue | grep -Eqi "ubuntu"; then
+        osRelease="ubuntu"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="bionic"
+    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    elif cat /proc/version | grep -Eqi "debian|raspbian"; then
+        osRelease="debian"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="buster"
+    elif cat /proc/version | grep -Eqi "ubuntu"; then
+        osRelease="ubuntu"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="bionic"
+    elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    fi
+
+    getLinuxOSVersion
+    checkArchitecture
+	checkCPU
+    virt_check
+
+    [[ -z $(echo $SHELL|grep zsh) ]] && osSystemShell="bash" || osSystemShell="zsh"
+
+    echo "OS info: ${osInfo}, ${osRelease}, ${osReleaseVersion}, ${osReleaseVersionNo}, ${osReleaseVersionCodeName}, ${osSystemShell}, ${osSystemPackage}, ${osSystemMdPath}, ${osVirtual}"
+}
+
+# 执行检测
+getLinuxOSRelease
