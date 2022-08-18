3bce5c499a6eea641e52a6ba5f02311e63ff3833
pangbobi <teddycool172@gmail.com>
2022-08-18 23:28:22 +0800

    更新多系统防火墙设置

---
+++ tools/defaultFirewall.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# 初始化防火墙设置
+Set_Firewall(){
+	# SSH 端口
+	sshPort=$(cat /etc/ssh/sshd_config | grep 'Port '|awk '{print $2}')
+
+	# 检测是否支持 IPV6
+	if [ "$(ifconfig | grep inet6)" ];then
+		IPV6="yes"
+	else
+		IPV6="no"
+	fi
+
+	# 开放 SSH 端口并设置防火墙开机启动
+	if [[ "$osSystemPackage" =~ "apt" ]];then
+		$osSystemPackage install -y ufw
+		if [ -f "/usr/sbin/ufw" ];then
+			[ "$IPV6" == "yes" ] && sed -i 's/IPV6=no/IPV6=yes/g' /etc/default/ufw
+			ufw allow $sshPort
+
+			# 允许开机自启
+			echo y|ufw enable
+			# 默认允许出站，拒绝入站
+			ufw default deny
+			ufw reload
+		fi
+		firewallTool="ufw"
+	else
+		if [ -f "/etc/init.d/iptables" ];then
+			choseIp6tables(){
+				iptableType=$1
+
+				$iptableType -I INPUT -p tcp -m state --state NEW -m tcp --dport $sshPort -j ACCEPT
+				$iptableType -I INPUT -p udp -m state --state NEW -m udp --dport $sshPort -j ACCEPT
+				
+				# 保持已连接的会话不断开
+				$iptableType -A INPUT -p icmp --icmp-type any -j ACCEPT
+				$iptableType -A INPUT -s localhost -d localhost -j ACCEPT
+				$iptableType -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+				$iptableType -P INPUT DROP
+				service $iptableType save
+			}
+			choseIp6tables 'iptables'
+			[ "$IPV6" == "yes" ] && choseIp6tables 'ip6tables'
+
+			# 持久化保存
+			clear && echo
+			echo -e "${Info}如有弹框，请务必选择 yes，按任意键继续..."
+			char=$(waitInput)
+			$osSystemPackage install -y iptables-persistent
+			
+			if [ "$(service iptables status | grep 'not running')" == "" ];then
+				service iptables restart
+				[ "$IPV6" == "yes" ] && service ip6tables restart
+			fi
+
+			netfilter-persistent save
+			[ "$IPV6" == "yes" ] && firewallTool="ip6tables" || firewallTool="iptables"
+		else
+			AliyunCheck=$(cat /etc/redhat-release|grep "Aliyun Linux")
+			[ "$AliyunCheck" ] && return
+			$osSystemPackage install -y firewalld
+			Centos8Check=$(cat /etc/redhat-release | grep ' 8.' | grep -iE 'centos|Red Hat')
+			[ "$Centos8Check" ] && $osSystemPackage reinstall -y python3-six
+
+			systemctl enable firewalld
+			systemctl start firewalld
+			firewall-cmd --set-default-zone=public > /dev/null 2>&1
+			firewall-cmd --permanent --zone=public --add-port=${sshPort}/tcp > /dev/null 2>&1
+			firewall-cmd --permanent --zone=public --add-port=${sshPort}/udp > /dev/null 2>&1
+			firewall-cmd --reload
+			firewallTool="firewall-cmd"
+		fi
+	fi
+
+	# 保存到配置文件
+	tmp=$(jq '.LoginInfo.sshPort="'$sshPort'"' $STATUS_FILE)
+	tmp=$(echo $tmp | jq '.IPV6="'$IPV6'"')
+	echo $tmp | jq '.FirewallTool="'$firewallTool'"' > tmp.json
+	mv tmp.json $STATUS_FILE
+}
+
+# 执行防火墙初始设置
+Set_Firewall
