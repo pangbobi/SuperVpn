e51b0b379ab7a530daa2358dcbb88df2dd90ae38
pangbobi <teddycool172@gmail.com>
2022-08-19 21:34:38 +0800

    结构优化

--- tools/removeShield.sh
+++ tools/removeShield.sh
@@ -22,17 +22,17 @@ Remove_Shield(){
 		rm -f uninstall.sh quartz_uninstall.sh
 
         # 屏蔽IP
-        setFirewall "close" "ip" "in" "140.205.201.0/28"
-        setFirewall "close" "ip" "in" "140.205.201.16/29"
-        setFirewall "close" "ip" "in" "140.205.201.32/28"
-        setFirewall "close" "ip" "in" "140.205.225.183/32"
-        setFirewall "close" "ip" "in" "140.205.225.184/29"
-        setFirewall "close" "ip" "in" "140.205.225.192/29"
-        setFirewall "close" "ip" "in" "140.205.225.195/32"
-        setFirewall "close" "ip" "in" "140.205.225.200/30"
-        setFirewall "close" "ip" "in" "140.205.225.204/32"
-        setFirewall "close" "ip" "in" "140.205.225.205/32"
-        setFirewall "close" "ip" "in" "140.205.225.206/32"
+        setFirewall "close" "ip" "140.205.201.0/28"
+        setFirewall "close" "ip" "140.205.201.16/29"
+        setFirewall "close" "ip" "140.205.201.32/28"
+        setFirewall "close" "ip" "140.205.225.183/32"
+        setFirewall "close" "ip" "140.205.225.184/29"
+        setFirewall "close" "ip" "140.205.225.192/29"
+        setFirewall "close" "ip" "140.205.225.195/32"
+        setFirewall "close" "ip" "140.205.225.200/30"
+        setFirewall "close" "ip" "140.205.225.204/32"
+        setFirewall "close" "ip" "140.205.225.205/32"
+        setFirewall "close" "ip" "140.205.225.206/32"
 	elif [[ $org =~ "Tencent" ]];then
 		# 腾讯云则卸载云盾："ps aux |grep -i agent |grep -v grep"
         chmod +x /usr/local/qcloud/stargate/admin/uninstall.sh
